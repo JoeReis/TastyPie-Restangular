9e3c4b3d3c4b6b8a5eaf52dd0a042ce35b877dd9
JoeReis <josephreis@gmail.com>
2014-01-22 20:39:54 -0700

    first page of tastypie tutorial done

--- tastypiedemo/urls.py
+++ tastypiedemo/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import patterns, include, url
-from tester.api import EntryResource
+from django.conf.urls import *
+from tastypie.api import Api
+from tester.api import EntryResource, UserResource
 
 from django.contrib import admin
 admin.autodiscover()
 
-entry_resource = EntryResource()
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(EntryResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,5 +15,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(entry_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )
--- tester/api.py
+++ tester/api.py
@@ -1,11 +1,19 @@
 # myapp/api.py
-from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from tester.models import Entry
+from django.contrib.auth.models import User
+from tastypie import fields
 
+class UserResource(ModelResource):
+	class Meta:
+		queryset = User.objects.all()
+		resource_name = 'user'
+		excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        allowed_methods = ['get']
 
 class EntryResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
     class Meta:
         queryset = Entry.objects.all()
         resource_name = 'entry'
-        authorization= Authorization()
